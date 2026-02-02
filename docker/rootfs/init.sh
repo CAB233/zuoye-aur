c126bf095aff006045d5f37f077d2e25223b8d52
CAB233 <yidaduizuoye@outlook.com>
2026-02-02 16:24:28 +0800

    ci: remove pacman-key command

--- docker/rootfs/init.sh
+++ docker/rootfs/init.sh
@@ -2,8 +2,6 @@
 set -e
 
 pacman-key --init
-pacman-key --recv-key 62FFE3FEF4158CF1 --keyserver keys.openpgp.org
-pacman-key --lsign-key 62FFE3FEF4158CF1
 pacman-key --populate
 
 # pacman -Syu --noconfirm
